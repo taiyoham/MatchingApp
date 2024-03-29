3ff9879d2a60a98bd5dede60c7e19a0b98698e29
Taiyo Hamamoto <taiyo.ocean5123ht@gmail.com>
2024-03-29 12:25:16 +0900

    Expoによるプロジェクト作成、Firebaseの初期設定を行いました。

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+const email = "taiyo.ocean5123ht@gmail.com";
+const password = "hoge";
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
 export default function App() {
   return (
